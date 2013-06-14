a80f44991e645835d2c63b581420eef29f634ca5
Christopher Phillips <christopher.phillips@audaxhealth.com>
2013-06-14 09:28:26 -0700

    Add plugin parameter

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -7,7 +7,8 @@
 #
 # Parameters:
 # - [$title]      - Title of plugin
-# - [$ensure]     - Should plugin be installed. Default is to remove.
+# - [$ensure]     - Should plugin be installed. Default is to remove
+# - [$plugin]     - The full name of the plugin which includes forward slash
 # - [$plugin_dir] - The name of the plugin directory to be installed
 #
 # Requires:
@@ -17,13 +18,19 @@
 #
 #  elasticsearch::plugin { 'foo-plugin':
 #    ensure     => 'present',
+#    plugin     => 'foo/plugin'
 #    plugin_dir => 'foo',
 #  }
 #
 define elasticsearch::plugin(
   $ensure     = 'present',
+  $plugin     = undef,
   $plugin_dir = undef
   ) {
+
+  if !$plugin {
+    fail("plugin undefined: specify plugin for ${title}")
+  }
 
   if !$plugin_dir {
     fail("plugin_dir undefined: specify plugin destination dir for ${title}")
@@ -32,7 +39,7 @@ define elasticsearch::plugin(
   case $ensure {
     'installed', 'present': {
       exec {"install-plugin-${title}":
-        command => "${elasticsearch::params::bin_plugin} -install ${title}",
+        command => "${elasticsearch::params::bin_plugin} -install ${plugin}",
         creates => "${elasticsearch::params::dir_plugins}/${plugin_dir}",
         require => Package['elasticsearch'],
         notify  => Service['elasticsearch'];
